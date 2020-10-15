333b8c7cb7157cd37415537e2940015e2e2f9ea5
Kylebear <32508474+KyleBear@users.noreply.github.com>
2020-10-15 22:49:44 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Customer from './components/Customer';
-//3개의 클래스 임포트
-//App.js는 javascript들의 기본 클래스, 스크립트 등을 정의
-// App.css 스타일링  
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import createSpacing from '@material-ui/core/styles/createSpacing';
+//3개의 클래스 임포트//App.js는 javascript들의 기본 클래스, 스크립트 등을 정의// App.css 스타일링  
+const styles = {
+  root: {
+  width: "100%",
+  marginTop : createSpacing.unit*3,
+  overflowX: "auto"
+  
+  },
+  table: {
+  minWidth: 1080
+  }
+  }; //style 변수만듬
+
 const customers = [{// 변수 
   'id' : 1, // id = 주소값으로 접근= 상수로 정의 , 문자열 x 
   'image' : 'http://placeimg.com/64/64/1', // 랜덤으로 이미지 보여줌. 
@@ -16,20 +34,44 @@ const customers = [{// 변수
 {
   'id' : 2, // id = 주소값으로 접근= 상수로 정의 , 문자열 x 
   'image' : 'http://placeimg.com/64/64/2', // 랜덤으로 이미지 보여줌. 
-  'name': '김정호',
+  'name': '개붕이',
   'birthday' : '951226',
   'gender' : '남자',
   'job' : '프로그래머'
-
-
+},
+{
+  'id' : 3, // id = 주소값으로 접근= 상수로 정의 , 문자열 x 
+  'image' : 'http://placeimg.com/64/64/3', // 랜덤으로 이미지 보여줌. 
+  'name': '흑운장',
+  'birthday' : '951227',
+  'gender' : '남자',
+  'job' : '프로게이머'
 }] // 배열로 바꿈 변수 customers 로 바꿈.
 
 //Customer클래스를 import 햇음.그리고 그안의 customer의 변수설정
 class App extends Component{
-  render() {
+  render() { 
+    const { classes } = this.props;
+// 앱 랜더링할때 classes변수를 지역변수로.
+
     return (
-      <div>   {//map 을 만듬. 
-     customers.map(c =>{ //특정변수에 접근해서 각 원소를 어떻게 처리 return하겟다 라고 처리.
+      //위쪽은 테이블의 속성을 출력 //바디는 내용읠 출력(컴포넌트)
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+        </TableHead>
+      <TableBody>
+        {customers.map(c =>
+     { //고객 컴포넌트를 만듬 
+       //특정변수에 접근해서 각 원소를 어떻게 처리 return하겟다 라고 처리.
        return (
        // 각 원소를 c 로 순회하도록 만듬. c라는 한명의 customer는 어떻게 출력되나?
        <Customer 
@@ -37,15 +79,18 @@ class App extends Component{
       id={c.id}
        image={c.image}
        name={c.name}
-       birthday={c.name}
+       birthday={c.birthday}
        gender={c.gender}
        job={c.job}
        />
          );
-     })
-   }
-</div>
+     }
+     )
+    }
+        </TableBody>
+      </Table>
+      </Paper> // 컴포넌트의 외부를 감싸기 위해 불러오는 컴포넌트.
    ); // 커스터머 클래스 불러옴. 커스터머 클래스의 name, job 등을 앱 컴포넌트로 재정의
 }
 }
-export default App;
+export default withStyles(styles)(App);//withStyles 임포트됫음. 현재 스크롤바
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,44 +1,28 @@
 import React from 'react'
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
     //일종의 클래스이자 라이브러리 리엑트 컴포넌트. APP.js인가?
-    render(){ 
-        return (
-           
             // 렌더링 실행문 - ()매개변수 없어도 실행 단 리턴문 필요.
             //계층적으로 구성하기위해 Customer Profile과 Customer info를 사용하도록 만듬.
              
-    <div>
-<CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>        
- <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>      
-    </div>
+    render(){ 
+        return (
+           <TableRow>
+               <TableCell>{this.props.id}</TableCell>
+               <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+               <TableCell>{this.props.name}</TableCell>
+               <TableCell>{this.props.birthday}</TableCell>
+               <TableCell>{this.props.gender}</TableCell>
+               <TableCell>{this.props.job}</TableCell>
+               </TableRow>
+
         //this가 가리키는것= Customer 클래스?  
         //prop가 가리키는것 - cost에서 전달받은 변수 customer의 객체값들을 반영. 
 
-    // 주소값이 아닌 '객체값'을  계층적으로 전달하는 구조.
+    // 주소값이 아닌 '객체값'(실존하는 값)을  계층적으로 전달하는 구조.
         )
 }
-}
-class CustomerProfile extends React.Component{
-    render() {
-    return (
-        <div>
-            <img src={this.props.image} alt= "profile" />
-            <h2>{this.props.name}({this.props.id})</h2>
-        </div>
-    ) // 렌더링의 리턴값 필수 // this 가 가리키는것 CustomerProfile인가 React.component 인가? 실행이 안될 시 alt값에 프로필 출력.
-} // 
-}
-class CustomerInfo extends React.Component { //사용자정보
-    render() {
-        return (
-            <div>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>
-            </div>
-        )
-    } 
-
 }
 export default Customer;
